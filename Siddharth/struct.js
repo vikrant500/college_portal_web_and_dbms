f974816e7f846dcee9ca46c5c3d9128de675c383
Kokushibou-27 <153221819+Kokushibou-27@users.noreply.github.com>
2024-04-08 00:01:08 +0530

    commit

--- Siddharth/struct.js
+++ Siddharth/struct.js
@@ -9,20 +9,22 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
-
+function Stud(){
 var email = document.getElementById("Email").value;
 var pass = document.getElementById("Password").value;
 
-if(email == "student@muj.manipal.edu" && pass == "password"){
-    window.href = 'student.html';
+if(email == "student@muj.manipal.edu" && pass == "password"){alert("login successful");
+    window.location.href = "https://youtube.com";  //assign('student.html');
 }else{
     alert("Invalid input. Please try again");
 }
-
+}
+function Prof(){
 var emails = document.getElementById("Emails").value;
 var passes = document.getElementById("Passwords").value;
 if(emails == "professor@muj.manipal.edu" && passes == "passwords"){
-    window.href = 'professor.html';
+    window.location.assign("professor.html");
 }else{
     alert("Invalid input. Please try again");
+}
 }
