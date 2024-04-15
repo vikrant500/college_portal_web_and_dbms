3d8d798b0daafe87da64ad2855cf8a8daffc66a8
vikrant500 <500vikrant@gmail.com>
2024-04-16 00:06:22 +0530

    test1

---
+++ Vikrant/dbms project 1/includes/dbh.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$dbname = "myfirstdatabase";
+$dbusername = "root";
+$dbpassword = "";
+
+try {
+    //code...
+
+    $pdo = new PDO("mysql:host=$host; dbname=$dbname", $dbusername, $dbpassword);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    
+    echo "Connection to the database failed: " . $e -> getMessage();
+}
